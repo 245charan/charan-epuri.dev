b3cc32ca5229436e5f765cf1254e3553fab3332c
Charan-Epuri <Charan.Epuri1@marist.edu>
2025-03-31 05:29:01 -0400

    page assets deploy

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import ProjectCard from '../components/projects/ProjectCard';
 import { media } from '../styles/Responsive';
+import marist from '../assets/images/projects/marist.webp';
+import sakai from '../assets/images/projects/sakai.jpg';
+import treeData from '../assets/images/projects/TreeData.gif';
+import rtsm from '../assets/images/projects/RTSM.gif';
+import bny from '../assets/images/projects/bny.png';
+import posrg from '../assets/images/projects/posrg.jpg';
+import placeholder from '../assets/images/projects/placeholder-project.png';
 
 const ProjectsContainer = styled.div`
 	margin-bottom: 3rem;
@@ -49,7 +56,7 @@ const Projects = () => {
 			description:
 				"Key contributor to rebuilding the Marist College's website during its university transition, focusing on accessibility compliance and user experience improvements. Helped implement solutions that reduced page load times and boosted search rankings while maintaining brand standards.",
 			// description: 'Built Marist University\'s flagship website in Liferay with micro-frontend architecture, combining CSS3/Bootstrap templates with Java backend services. Achieved perfect accessibility compliance, 50% faster page loads, and 30% SEO improvement through structured data implementation.',
-			image: 'src/assets/images/projects/marist.webp',
+			image: marist,
 			techStack: [
 				'Liferay',
 				'Accessibility',
@@ -61,21 +68,21 @@ const Projects = () => {
 				'Java',
 				'Spring Boot',
 			],
-			github: 'https://github.com/245charan/tree-data-form-builder',
+			// github: 'https://github.com/245charan/tree-data-form-builder',
 			liveDemo: 'https://marist.edu',
 		},
 		{
 			title: 'Sakai LMS Enhancement',
 			description:
 				'Contributed to the Sakai LMS open-source project by resolving bugs and enhancing accessibility through component-based modernization. Conducted comprehensive testing including unit, integration, and user acceptance testing (UAT), documenting defects in JIRA with detailed reproduction workflows. Rebuilt legacy pages using component-driven design.',
-			image: 'src/assets/images/projects/sakai.jpg',
+			image: sakai,
 			techStack: [
 				'Tailwind',
 				'Accessibility',
 				'WCAG',
 				'Bug Fix',
 				'UI modernization',
-        'Jira',
+				'Jira',
 			],
 			github: 'https://github.com/sakaiproject/sakai',
 			liveDemo: 'https://qa2-us.nightly.sakaiproject.org/',
@@ -84,7 +91,7 @@ const Projects = () => {
 			title: 'Tree Data Form Builder App',
 			description:
 				'Admin-managed hierarchical data collection system with role-based access (Admin/User), dynamic fields, reusable templates, and automated email workflows via Power Automate. Implemented using adjacency list modeling for efficient tree data structure.',
-			image: 'src/assets/images/projects/TreeData.gif',
+			image: treeData,
 			techStack: [
 				'React',
 				'Redux',
@@ -100,8 +107,7 @@ const Projects = () => {
 			title: 'Real-Time Stock Prices Monitoring',
 			description:
 				'Web application for real-time stock price tracking, trend prediction, and customizable watchlists with price-threshold alerts via Telegram and WebSocket notifications. Implemented using Django Channels for WebSockets and Celery for background tasks.',
-        image: 'src/assets/images/projects/RTSM.gif',
-
+			image: rtsm,
 			techStack: [
 				'Django',
 				'Django Channels',
@@ -118,7 +124,7 @@ const Projects = () => {
 			title: 'Beverage Patashala App',
 			description:
 				'Full-featured order management system where admins manage order queues for customers. Features include hosting, multi-layout support, authentication, lazy loading, responsive UI, and bulk orders/transfers. Implemented with Firebase Realtime Database for real-time updates.',
-			image: 'src/assets/images/projects/placeholder-project.png',
+			image: placeholder,
 			techStack: ['React', 'Redux', 'Bootstrap', 'Firebase', 'Webpack'],
 			github: 'https://github.com/245charan/beverage-patashala',
 			liveDemo: 'https://beveragepatashala.web.app/',
@@ -127,15 +133,15 @@ const Projects = () => {
 			title: 'BNY Mellon Fund Management Application',
 			description:
 				'Enterprise-level fund management and accounting application built with micro-frontend architecture. Reduced package load time from 15 minutes to <1 second and achieved ~95% test coverage. Implemented CI/CD pipelines using AWS services, reducing deployment time by 50%.',
-			image: 'src/assets/images/projects/bny.png',
+			image: bny,
 			techStack: [
 				'React',
 				'Redux',
 				'Micro-Frontend',
-        'Styled-components',
+				'Styled-components',
 				'Java ',
 				'Spring Boot',
-        'Junit',
+				'Junit',
 				'Maven',
 				'AWS',
 				'Jest',
@@ -148,7 +154,7 @@ const Projects = () => {
 			title: 'POSRG E-commerce Platform',
 			description:
 				'Complete web application developed independently from setting up Cloudways hosting to configuring domains and optimizing SEO visibility. Implemented custom UI components and shortcodes based on Figma designs. Achieved 90+ scores in Google Lighthouse for performance, accessibility, and SEO.',
-			image: 'src/assets/images/projects/posrg.jpg',
+			image: posrg,
 			techStack: [
 				'WordPress',
 				'Custom Theme',
