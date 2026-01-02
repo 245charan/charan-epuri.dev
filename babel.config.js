d773c5da780b80a8e3d5b14fe6b9acac73aceeba
Charan-Epuri <Charan.Epuri1@marist.edu>
2026-01-02 18:35:05 -0500

    Updates

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js
-const config = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }]
-  ],
-};
-
-export default config;
